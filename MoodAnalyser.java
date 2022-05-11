776f094019b2f5026dfcf158c2781a325b310d28
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-05-11 22:58:02 +0530

    added Refactor_code using constructor

---
+++ MoodAnalyser.java
@@ -0,0 +1,15 @@
+package moodanalyser;
+
+public class MoodAnalyser {
+    String message;
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+    public String analyseMood() {
+        if (message.contains("sad")) {
+            return "SAD";
+        } else
+            return "HAPPY";
+    }
+}
+
