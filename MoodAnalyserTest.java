03c13e7ad378c9039e11b557af42b8720e1e2e95
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-05-11 22:52:25 +0530

    added TC_1.2_return_happy

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,20 @@
+package MoodAnalyizer;
+import moodanalyser.MoodAnalyser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+public class MoodAnalyserTest {
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser=new MoodAnalyser();
+        String result=moodAnalyser.analyseMood("I am in sad mood");
+        System.out.print(result);
+        Assertions.assertEquals(result, "SAD");
+    }
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser=new MoodAnalyser();
+        String result=moodAnalyser.analyseMood("I am in any mood");
+        Assertions.assertEquals(result, "HAPPY");
+    }
+}
