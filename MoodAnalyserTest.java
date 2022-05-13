dfcd61501701eaecca0c73c8347443b1a6ad949f
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-05-13 14:11:23 +0530

    added UC2_Null_pointer_ex

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,22 @@
+package MoodAnalyizer;
+import moodanalyser.MoodAnalyser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+public class MoodAnalyserTest {
+        @Test
+        public void givenMessage_WhenProper_ShouldReturnSad(){
+            MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+            String result = moodAnalyser.analyseMood();
+            System.out.println(result);
+            Assertions.assertEquals("SAD" , result);
+        }
+        @Test
+        public void givenMessage_WhenProper_ShouldReturnHappy(){
+            MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+            String result = moodAnalyser.analyseMood();
+            System.out.println(result);
+            Assertions.assertEquals("SAD" , result);
+        }
+
+    }
